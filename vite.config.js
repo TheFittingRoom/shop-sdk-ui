350fc5a0a36ef2a31f058eef17333a2802ffc031
codeliger <codeliger@protonmail.com>
2025-11-17 17:08:33 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode, command }) => {
-  // For dev server, we don't want the library build configuration interfering
   if (command === 'serve') { // dev mode
     return {
       server: {
@@ -20,25 +19,43 @@ export default defineConfig(({ mode, command }) => {
       },
     }
   } else { // build mode
-    return {
-      build: {
-        lib: {
-          entry: resolve(__dirname, 'src/index.ts'),
-          name: 'TheFittingRoom',
-          fileName: 'index',
+    if (mode === 'production') {
+      return {
+        build: {
+          lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'TheFittingRoom',
+            fileName: 'index',
+          },
+          minify: 'esbuild',
         },
-        minify: mode === 'production' ? 'esbuild' : false,
-      },
-      server: {
-        port: 5173,
-        host: true,
-        cors: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        server: {
+          port: 5173,
+          host: true,
+          cors: true,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          },
         },
-      },
+      }
+    } else {
+      return {
+        build: {
+          minify: false,
+        },
+        server: {
+          port: 5173,
+          host: true,
+          cors: true,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          },
+        },
+      }
     }
   }
 })
