f5d245ca3dd11354202b90156f47e5f551fc4b82
codeliger <codeliger@protonmail.com>
2025-12-04 02:34:00 -0500

    fix vite to build dev/prod properly

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,39 @@
 import { resolve } from 'path'
-import { defineConfig, resolveEnvPrefix } from 'vite'
+import { defineConfig } from 'vite'
+
+const server = {
+  port: 5173,
+  host: true,
+  cors: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+  },
+}
+
+const lib = {
+  lib: {
+    entry: resolve('src/index.ts'),
+    name: 'TheFittingRoom',
+    fileName: 'index',
+  },
+  minify: false,
+}
 
 export default defineConfig(({ mode, command }) => {
-  if (command === 'serve') { // dev mode
+  if (command === 'serve') {
     return {
       server: {
-        port: 5173,
-        host: true,
-        cors: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-        },
-        watch: {
-          usePolling: true,
-        }
+        ...server,
+        watch: { usePolling: true },
       },
     }
-  } else { // build mode
-    if (mode === 'production') {
-      return {
-        build: {
-          lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'TheFittingRoom',
-            fileName: 'index',
-          },
-          minify: false,
-        },
-        server: {
-          port: 5173,
-          host: true,
-          cors: true,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-          },
-        },
-      }
-    } else {
-      return {
-        build: {
-          minify: false,
-        },
-        server: {
-          port: 5173,
-          host: true,
-          cors: true,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-          },
-        },
-      }
-    }
   }
+
+  if (mode === 'production' || process.env.CI) {
+    return { build: lib, server }
+  }
+
+  return { build: { minify: false }, server }
 })
