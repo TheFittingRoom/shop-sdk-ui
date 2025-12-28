57a3bafe8105e31ea9ad1fab41863d35781adff7
Ky Patterson <ky@achronos.ca>
2025-12-28 04:44:18 -0500

    cache size rec api response

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,24 +1,37 @@
 import {
-  FirestoreColorwaySizeAsset,
-  FirestoreGarmentCategory,
-  FirestoreMeasurementLocation,
-  FirestoreStyle,
-  FirestoreStyleCategory,
-  FirestoreStyleGarmentCategory,
-  FirestoreUser,
+  // FirestoreColorwaySizeAsset,
+  // FirestoreGarmentCategory,
+  // FirestoreMeasurementLocation,
+  // FirestoreStyle,
+  // FirestoreStyleCategory,
+  // FirestoreStyleGarmentCategory,
+  // FirestoreUser,
   SizeFitRecommendation,
 } from '@/api/gen/responses'
 import { EnvName, getConfig } from '@/lib/config'
 import { getAuthManager } from '@/lib/firebase'
+import { useMainStore } from '@/lib/store'
 
 let baseUrl: string
+let responseCache: { [key: string]: unknown } = {}
 
 export function _init(environment: EnvName) {
   const config = getConfig(environment)
   baseUrl = config.api.baseUrl
+
+  useMainStore.subscribe((state, prevState) => {
+    if (state.userIsLoggedIn !== prevState.userIsLoggedIn) {
+      clearApiCache()
+    }
+  })
+}
+
+export function clearApiCache() {
+  responseCache = {}
 }
 
 interface ApiRequestParams {
+  useCache?: boolean
   useToken?: boolean
   method: RequestInit['method']
   endpoint: string
@@ -27,8 +40,14 @@ interface ApiRequestParams {
 
 async function execApiRequest<T>(params: ApiRequestParams): Promise<T> {
   const authManager = getAuthManager()
-  const { useToken, method, endpoint, body } = params
-  const url = `${baseUrl}/v1${endpoint}`
+  const { useCache, useToken, method, endpoint, body } = params
+
+  const url = `${baseUrl}${endpoint}`
+
+  if (useCache && responseCache[url]) {
+    return responseCache[url] as T
+  }
+
   const options: RequestInit = {
     method,
     headers: { 'Content-Type': 'application/json' },
@@ -49,13 +68,19 @@ async function execApiRequest<T>(params: ApiRequestParams): Promise<T> {
     throw new Error(`API request failed with status ${response.status}`)
   }
   const data = (await response.json()) as T
+
+  if (useCache) {
+    responseCache[url] = data
+  }
+
   return data
 }
 
 export async function getSizeRecommendation(styleId: number): Promise<SizeFitRecommendation> {
   return execApiRequest<SizeFitRecommendation>({
+    useCache: true,
     useToken: true,
     method: 'GET',
-    endpoint: `/styles/${styleId}/recommendation`,
+    endpoint: `/v1/styles/${styleId}/recommendation`,
   })
 }
