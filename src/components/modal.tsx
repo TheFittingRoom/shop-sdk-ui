6e74b213fd85f211c18ded4992f3b3d7ae38657a
Ky Patterson <ky@achronos.ca>
2025-12-31 09:47:10 -0500

    rebuild modal components

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -5,111 +5,23 @@ import { ArrowBackIcon, CloseIcon } from '@/lib/asset'
 import { useMainStore } from '@/lib/store'
 import { useCss } from '@/lib/theme'
 
-export type ModalVariant = 'medium' | 'large' | 'full-screen'
-
-export interface ModalProps {
+export interface ModalFrameProps {
   isOpen: boolean
   onRequestClose: () => void
-  title: ReactNode
-  variant: ModalVariant
-  onBackClick?: () => void
+  contentStyle: CSSProperties
   children: ReactNode
 }
 
-const VARIANT_FRAME_CONTENT_STYLES: Record<ModalVariant, CSSProperties> = {
-  medium: {
-    width: '540px',
-    height: undefined,
-  },
-  large: {
-    width: undefined,
-    height: undefined,
-  },
-  'full-screen': {
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh',
-  },
-}
-
-export function Modal({ isOpen, onRequestClose, title, variant, onBackClick, children }: ModalProps) {
-  const deviceView = useMainStore((state) => state.deviceView)
-  if (deviceView === 'mobile') {
-    variant = 'full-screen'
-  }
-  const css = useCss((theme) => {
-    const variantFrameContentStyles = VARIANT_FRAME_CONTENT_STYLES[variant]
-    return {
-      frameOverlay: {
-        zIndex: 1000,
-      },
-      frameContent: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        backgroundColor: '#FFFFFF',
-        borderColor: theme.color_tfr_800,
-        borderStyle: 'solid',
-        borderWidth: '1px',
-        padding: '16px',
-        ...variantFrameContentStyles,
-      },
-      titlebar: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      },
-      titlebarBackButton: {
-        background: 'none',
-        border: 'none',
-        padding: 0,
-        cursor: 'pointer',       
-      },
-      titlebarBackIcon: {
-        width: '24px',
-        height: '24px',
-      },
-      titlebarContent: {},
-      titlebarCloseButton: {
-        background: 'none',
-        border: 'none',
-        padding: 0,
-        cursor: 'pointer',
-      },
-      titlebarCloseIcon: {
-        width: '16px',
-        height: '16px',
-      },
-      body: {
-        margin: '16px',
-      },
-    }
-  })
-  let backNode: ReactNode
-  if (onBackClick) {
-    backNode = (
-      <button onClick={onBackClick} aria-label="Back" css={css.titlebarBackButton}>
-        <ArrowBackIcon css={css.titlebarBackIcon} />
-      </button>
-    )
-  } else {
-    backNode = <div>&nbsp;</div>
+export function ModalFrame({ isOpen, onRequestClose, contentStyle, children }: ModalFrameProps) {
+  const styleProp: { overlay: CSSProperties; content: CSSProperties } = {
+    overlay: {
+      zIndex: 1000,
+    },
+    content: contentStyle,
   }
   return (
-    <ModalBase
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      style={{ overlay: css.frameOverlay, content: css.frameContent }}
-      bodyOpenClassName="tfr-modal-open"
-    >
-      <div css={css.titlebar}>
-        {backNode}
-        <div css={css.titlebarContent}>{title}</div>
-        <button onClick={onRequestClose} aria-label="Close modal" css={css.titlebarCloseButton}>
-          <CloseIcon css={css.titlebarCloseIcon} />
-        </button>
-      </div>
-      <div css={css.body}>{children}</div>
+    <ModalBase isOpen={isOpen} onRequestClose={onRequestClose} style={styleProp} bodyOpenClassName="tfr-modal-open">
+      {children}
     </ModalBase>
   )
 }
@@ -122,9 +34,57 @@ export interface ContentModalProps {
 }
 
 export function ContentModal({ onRequestClose, title, onBackClick, children }: ContentModalProps) {
-  const css = useCss((_theme) => ({
-    title: {
+  const deviceView = useMainStore((state) => state.deviceView)
+  const css = useCss((theme) => ({
+    frameContentBase: {
+      width: '540px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      backgroundColor: '#FFFFFF',
+      borderColor: theme.color_tfr_800,
+      borderStyle: 'solid',
+      borderWidth: '1px',
+      padding: '16px',
+    },
+    frameContentFullScreen: {
+      top: 0,
+      left: 0,
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: '#FFFFFF',
+      padding: '16px',
+    },
+    titlebar: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    titlebarBackButton: {
+      background: 'none',
+      border: 'none',
+      padding: 0,
+      cursor: 'pointer',
+    },
+    titlebarBackIcon: {
+      width: '24px',
+      height: '24px',
+    },
+    titlebarContent: {},
+    titlebarCloseButton: {
+      background: 'none',
+      border: 'none',
+      padding: 0,
+      cursor: 'pointer',
+    },
+    titlebarCloseIcon: {
+      width: '16px',
+      height: '16px',
+    },
+    titleText: {
       textTransform: 'uppercase',
+    },
+    body: {
+      margin: '16px',
     },
     contentContainer: {
       maxWidth: '390px',
@@ -136,23 +96,44 @@ export function ContentModal({ onRequestClose, title, onBackClick, children }: C
       alignItems: 'center',
     },
   }))
+
   let titleNode: ReactNode
   if (typeof title === 'string') {
-    titleNode = <Text variant="base" css={css.title}>{title}</Text>
+    titleNode = (
+      <Text variant="base" css={css.titleText}>
+        {title}
+      </Text>
+    )
   } else {
     titleNode = title
   }
+  let backNode: ReactNode
+  if (onBackClick) {
+    backNode = (
+      <button onClick={onBackClick} aria-label="Back" css={css.titlebarBackButton}>
+        <ArrowBackIcon css={css.titlebarBackIcon} />
+      </button>
+    )
+  } else {
+    backNode = <div>&nbsp;</div>
+  }
+
   return (
-    <Modal
+    <ModalFrame
       isOpen
       onRequestClose={onRequestClose}
-      variant="medium"
-      onBackClick={onBackClick}
-      title={titleNode}
+      contentStyle={deviceView === 'mobile' ? css.frameContentFullScreen : css.frameContentBase}
     >
-      <div css={css.contentContainer}>
-        {children}
+      <div css={css.titlebar}>
+        {backNode}
+        <div css={css.titlebarContent}>{title}</div>
+        <button onClick={onRequestClose} aria-label="Close modal" css={css.titlebarCloseButton}>
+          <CloseIcon css={css.titlebarCloseIcon} />
+        </button>
       </div>
-    </Modal>
+      <div css={css.body}>
+        <div css={css.contentContainer}>{children}</div>
+      </div>
+    </ModalFrame>
   )
 }
